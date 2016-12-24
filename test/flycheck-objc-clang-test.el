3db6a6100c5c229f9b5340ed6532a69b93ba7343
Goichi Hirakawa <gooichi@gyazsquare.com>
2016-12-24 19:37:39 +0900

    refs #7 Add tests

---
+++ test/flycheck-objc-clang-test.el
@@ -0,0 +1,88 @@
+;;; flycheck-objc-clang-test.el --- Flycheck Objective-C Clang: Test cases
+
+;; Copyright (c) 2016 GyazSquare Inc.
+
+;; Author: Goichi Hirakawa <gooichi@gyazsquare.com>
+;; URL: https://github.com/GyazSquare/flycheck-objc-clang
+
+;; This file is not part of GNU Emacs.
+
+;; MIT License
+
+;; Permission is hereby granted, free of charge, to any person obtaining
+;; a copy of this software and associated documentation files (the
+;; "Software"), to deal in the Software without restriction, including
+;; without limitation the rights to use, copy, modify, merge, publish,
+;; distribute, sublicense, and/or sell copies of the Software, and to
+;; permit persons to whom the Software is furnished to do so, subject to
+;; the following conditions:
+
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Commentary:
+
+;; Test cases Flycheck Objective-C Clang.
+
+;;; Code:
+
+(require 'flycheck-ert)
+(require 'flycheck-objc-clang)
+
+(message "Running tests on Emacs %s" emacs-version)
+
+(defconst flycheck-objc-clang-test-directory
+  (let ((filename (if load-in-progress load-file-name (buffer-file-name))))
+    (expand-file-name "test/" (locate-dominating-file filename "Cask")))
+  "Test suite directory, for resource loading.")
+
+(flycheck-ert-def-checker-test objc-clang objc warnings
+  (let ((flycheck-checkers '(objc-clang)))
+    (flycheck-ert-should-syntax-check
+     "expressions.m" 'objc-mode
+     '(4 3 warning "expression result unused" :checker objc-clang))))
+
+(flycheck-ert-def-checker-test objc-clang objc errors
+  (let ((flycheck-checkers '(objc-clang)))
+    (flycheck-ert-should-syntax-check
+     "missing-closing-rbrace.m" 'objc-mode
+     '(3 16 error "'@end' appears where closing brace '}' is expected"
+         :checker objc-clang))))
+
+(flycheck-ert-def-checker-test objc-clang objc warnings+notes
+  (let ((flycheck-checkers '(objc-clang)))
+    (flycheck-ert-should-syntax-check
+     "unqualified-to-qualified-class-warn.m" 'objc-mode
+     '(21 66 info "passing argument to parameter 'instance' here"
+          :checker objc-clang)
+     '(27 47 warning "incompatible pointer types passing 'AClass *' to parameter of type 'AClass<Fooable> *'"
+          :checker objc-clang))))
+
+(flycheck-ert-def-checker-test objc-clang objc errors+notes
+  (let ((flycheck-checkers '(objc-clang)))
+    (flycheck-ert-should-syntax-check
+     "missing-end.m" 'objc-mode
+     '(3 1 info "class started here" :checker objc-clang)
+     '(6 3 error "expected an Objective-C directive after '@'"
+         :checker objc-clang)
+     '(7 36 error "missing '@end'" :checker objc-clang))))
+
+(flycheck-ert-initialize flycheck-objc-clang-test-directory)
+
+(provide 'flycheck-objc-clang-test)
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+
+;;; flycheck-objc-clang-test.el ends here
